379572e86b517153a5d0de4dd3145219ccaa236b
sshibinthomass <sshibinthomass@gmail.com>
2025-12-30 23:39:36 +0100

    fix: chunk fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'framer-motion', 'i18next', 'react-i18next'],
+        },
+      },
+    },
+  },
 })
